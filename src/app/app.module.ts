742d5db21d69a682fc499717b505ef1d4e040ffe
Sonia Muniz <soninhamusi@gmail.com>
2024-09-08 10:10:20 -0400

    projeto-inicial-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Comp2Component } from './comp2/comp2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule
